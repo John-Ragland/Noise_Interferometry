752030a809bc5e231fb5a8c0538cdd63184eec25
John Ragland <jhrag@uw.edu>
2020-10-12 17:30:21 +0000

    added gitignore

--- noise_interferometry.py
+++ noise_interferometry.py
@@ -8,15 +8,18 @@ import datetime
 import numpy as np
 from obspy import read,Stream, Trace
 from obspy.core import UTCDateTime
-from ooipy.hydrophone import Noise_Interferometry
+from ooipy.hydrophone import Noise_Interferometry as NI
 import pickle
 
-num_periods = 360
-avg_time = 60 #minutes
+num_periods = 500 
+avg_time = 60  #minutes
 start_time = datetime.datetime(2017,3,10,0,0,0) # time of first sample
 node1 = '/LJ01C'
 node2 = '/PC01A'
-filter_data = True
-W = 30
+filter_cutoffs = [12, 30]
+W = 90
+htype = 'broadband'
+whiten= True
+kstart= 408
 
-Noise_Interferometry.calculate_NCF_loop(num_periods, node1, node2, avg_time, start_time, W=30, verbose=True)
+NI.calculate_NCF_loop(num_periods, node1, node2, avg_time, start_time, W, filter_cutoffs, verbose=True, whiten=whiten, htype=htype, kstart=kstart)
