dc93e14c8d8c6db065ee959c78c35c015e8a03ff
John-Ragland <jhrag@uw.edu>
2020-11-13 13:24:49 -0800

    general edits

--- noise_interferometry.py
+++ noise_interferometry.py
@@ -11,17 +11,17 @@ from obspy.core import UTCDateTime
 from ooipy.hydrophone import Noise_Interferometry as NI
 import pickle
 
-num_periods = 1
+num_periods = 300
 avg_time = 60  #minutes
 start_time = datetime.datetime(2017,3,10,0,0,0) # time of first sample
 node1 = 'Central_Caldera'
-node2 = 'Axial_Base'
-filter_cutoffs = np.array([1, 90])
-W = 90
+node2 = 'Eastern_Caldera'
+filter_cutoffs = np.array([0.5, 30])
+W = 30
 htype = 'low_frequency'
 whiten= True
 kstart= 0
-other_notes = 'zero phase filtering'
+other_notes = 'Experimenting with smaller frequency band'
 sp_method = 'sabra'
 
 NI.calculate_NCF_loop(num_periods, node1, node2, avg_time, start_time, W, filter_cutoffs, verbose=True, whiten=whiten, htype=htype, kstart=kstart, sp_method = sp_method, other_notes=other_notes)
