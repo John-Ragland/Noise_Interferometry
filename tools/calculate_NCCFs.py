697e10a3f3743700b50992d119b6edb4a9e14f6a
John Ragland <jhrag@uw.edu>
2020-12-28 09:11:03 -0800

    updated importing for calculate_NCCFs.py

--- tools/calculate_NCCFs.py
+++ tools/calculate_NCCFs.py
@@ -1,19 +1,22 @@
 import os
 import sys
-cwd = os.getcwd()
-ooipy_dir = os.path.dirname(cwd) + '/ooipy'
-sys.path.append(ooipy_dir)
 from matplotlib import pyplot as plt
 import datetime
 import numpy as np
 from obspy import read,Stream, Trace
 from obspy.core import UTCDateTime
-from ooipy.hydrophone import Noise_Interferometry as NI
 import pickle
 
-num_periods = 8760
+cwd = os.getcwd()
+ni_dir = os.path.dirname(cwd)
+sys.path.append(ni_dir)
+
+from Noise_Interferometry import calculate
+# sys.path.append(ooipy_dir)
+
+num_periods = 5
 avg_time = 60  #minutes 
-start_time = datetime.datetime(2018,1,1,0,0,0) # time of first sample
+start_time = datetime.datetime(2017,1,1,0,0,0) # time of first sample
 node1 = 'Central_Caldera'
 node2 = 'Eastern_Caldera'
 filter_cutoffs = np.array([1, 90])
@@ -24,5 +27,5 @@ kstart = 0
 other_notes = 'Butterworth and Hann Window for whitening. Longterm Analysis'
 sp_method = 'sabra_b'
 
-NI.calculate_NCF_loop(num_periods, node1, node2, avg_time, start_time, W, filter_cutoffs, verbose=True, whiten=whiten, htype=htype, kstart=kstart, sp_method = sp_method, other_notes=other_notes)
+calculate.calculate_NCF_loop(num_periods, node1, node2, avg_time, start_time, W, filter_cutoffs, verbose=True, whiten=whiten, htype=htype, kstart=kstart, sp_method = sp_method, other_notes=other_notes)
 
